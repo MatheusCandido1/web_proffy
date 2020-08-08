3ba4299d9fbd5f013ed454a287d361e3fcc05a14
Matheus <matheus9bh@hotmail.com>
2020-08-08 16:29:55 -0300

    add-textarea

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -6,6 +6,7 @@ import './style.css'
 import Input from '../../components/Input';
 
 import warningIcon from '../../assets/images/icons/warning.svg'
+import Textarea from '../../components/Textarea';
 
 function TeacherForm() {
     return (
@@ -21,6 +22,7 @@ function TeacherForm() {
                 <Input name="name" label="Nome completo" />
                 <Input name="avatar" label="Avatar" />
                 <Input name="whatsapp" label="WhatsApp" />
+                <Textarea name="bio" label="Biografia"/>
             </fieldset>
 
             <fieldset>
